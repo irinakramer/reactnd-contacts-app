11dce22755e81fd1f8974ada721692b92f605aed
Irina Kramer <irakramer@gmail.com>
2021-03-14 19:48:01 -0700

    feat: Fetch data from ContactsAPI thru lifecycle method

--- src/App.js
+++ src/App.js
@@ -1,50 +1,20 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
-
-// const contacts = [
-//   {
-//     "id": "karen",
-//     "name": "Karen Isgrigg",
-//     "handle": "karen_isgrigg",
-//     "avatarURL": "http://localhost:5001/karen.jpg"
-//   },
-//   {
-//     "id": "richard",
-//     "name": "Richard Kalehoff",
-//     "handle": "richardkalehoff",
-//     "avatarURL": "http://localhost:5001/richard.jpg"
-//   },
-//   {
-//     "id": "tyler",
-//     "name": "Tyler McGinnis",
-//     "handle": "tylermcginnis",
-//     "avatarURL": "http://localhost:5001/tyler.jpg"
-//   }
-//  ];
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "karen",
-        "name": "Karen Isgrigg",
-        "handle": "karen_isgrigg",
-        "avatarURL": "http://localhost:5001/karen.jpg"
-      },
-      {
-        "id": "richard",
-        "name": "Richard Kalehoff",
-        "handle": "richardkalehoff",
-        "avatarURL": "http://localhost:5001/richard.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "handle": "tylermcginnis",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-     ]
+    contacts: []
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
 
   // return a new array that filters out the contact we passed in
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -62,7 +62,7 @@ class ListContacts extends Component {
                             }}></div>
                             <div className="contact-details">
                                 <p>{contact.name}</p>
-                                <p>@{contact.handle}</p>
+                                <p>{contact.handle}</p>
                             </div>
                             <button 
                                 onClick={() => this.props.onDeleteContact(contact)}
